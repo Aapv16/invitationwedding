03cd724185b8777a3c29d7b4eab82f236be55868
Aapv16 <afrianusvicky38@gmail.com>
2025-08-17 00:39:57 +0700

    2

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/", // ganti dengan nama repo kamu
+  base: "/invitationwedding/", // ganti dengan nama repo kamu
   server: {
     port: 3000,
   },
